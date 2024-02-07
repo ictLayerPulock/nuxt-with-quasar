bb3c871c0cc1c8d73b4441c42356b6ad9b119ac7
ictLayerPulock <157199030+ictLayerPulock@users.noreply.github.com>
2024-02-07 19:54:22 +0600

    installetion quasar first time

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,17 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
+  <div class="q-pa-md row mx-auto items-start q-gutter-md">
+    <q-card dark bordered class="bg-grey-5 my-card">
+      <q-card-section>
+        <div class="text-h6">User Form</div>
+        <div class="text-subtitle2">Create by Pulock</div>
+      </q-card-section>
 
+      <q-separator dark inset />
+
+      <q-card-section>
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <q-input
+            filled
+            v-model="name"
+            label="Your name *"
+            hint="Name and surname"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please type something',
+            ]"
+          />
+          <q-input
+            filled
+            v-model="email"
+            label="Your email *"
+            hint="email and suremail"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please type something',
+            ]"
+          />
+
+          <q-input
+            filled
+            type="number"
+            v-model="age"
+            label="Your age *"
+            lazy-rules
+            :rules="[
+              (val) => (val !== null && val !== '') || 'Please type your age',
+              (val) => (val > 0 && val < 100) || 'Please type a real age',
+            ]"
+          />
+
+          <q-toggle v-model="accept" label="I accept the license and terms" />
+
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+            <q-btn
+              label="Reset"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+            />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
